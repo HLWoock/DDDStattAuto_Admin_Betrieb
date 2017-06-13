b4bb3783bb07a6b1bd2ccefed98fa1278e687771
HermannW <HermannW@Noose-1099.oosede.local>
2017-06-13 10:19:40 +0200

    initial

---
+++ src/main/java/de/woock/ddd/stattauto/gui/betrieb/DddStattAuto_Admin_Betrieb.java
@@ -0,0 +1,19 @@
+package de.woock.ddd.stattauto.gui.betrieb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import be.ordina.msdashboard.EnableMicroservicesDashboardServer;
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
+@EnableAdminServer
+@EnableMicroservicesDashboardServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class DddStattAuto_Admin_Betrieb {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddStattAuto_Admin_Betrieb.class, args);
+	}
+}
